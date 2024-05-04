cd4001bf9370c38e7a4a2c0d44c3634c86353367
Techtide18 <168283173+Techtide18@users.noreply.github.com>
2024-05-04 13:47:24 +0530

    REMOVING DUMMY FILES

--- src/main/java/com/businessAds/integration/repositories/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.businessAds.integration.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Long, Long> {
-
-
-
-}
